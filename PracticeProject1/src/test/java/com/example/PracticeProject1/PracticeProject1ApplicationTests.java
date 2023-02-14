a520a3b32dc6831e212341de0d6a26ef0eb09ae0
lhnhan <hoangnhan2506@gmail.com>
2023-02-14 14:38:20 +0700

    init project

---
+++ PracticeProject1/src/test/java/com/example/PracticeProject1/PracticeProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PracticeProject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticeProject1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
